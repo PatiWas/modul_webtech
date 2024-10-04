b8079a6bf4d3fea5cd2af828ca56a28d3abea97f
PatiWas <patricia.wassmer@stud.sfgz.ch>
2024-10-04 20:04:58 +0200

    Plakat Final

---
+++ Webtech_05/graphis_pw.js
@@ -0,0 +1,41 @@
+// let aboxon = class("abox on");
+// function pushed() {
+//     aboxon
+// }
+
+
+function changeClass() {
+    const rbox = document.getElementsByClassName('rbox');
+
+    // Check if the element has 'new-class'
+    if (rbox.classList.contains('on')) {
+        // Remove 'new-class' and add 'initial-class'
+        rbox.classList.remove('on');
+        rbox.classList.add('rbox');
+    } else {
+        // Remove 'initial-class' and add 'new-class'
+        rbox.classList.remove('rbox');
+        rbox.classList.add('on');
+    }
+}
+
+// let aboxon = class("abox on");
+// function pushed() {
+//     aboxon
+// }
+
+
+function changeClass() {
+    const rbox = document.getElementsByClassName('rbox');
+
+    // Check if the element has 'new-class'
+    if (rbox.classList.contains('on')) {
+        // Remove 'new-class' and add 'initial-class'
+        rbox.classList.remove('on');
+        rbox.classList.add('rbox');
+    } else {
+        // Remove 'initial-class' and add 'new-class'
+        rbox.classList.remove('rbox');
+        rbox.classList.add('on');
+    }
+}
